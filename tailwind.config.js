5f7a4400ad6e1c8ff85693683d2ecbf48fe68b68
Abduqodir65 <abduqodirgithub@gmail.com>
2024-12-20 14:34:37 +0500

    fixed bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: 'class', // Dark rejimni faollashtirish uchun
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Fayl joylashuvini belgilang
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBg: '#1a202c',
+        darkText: '#cbd5e0',
+        lightBg: '#edf2f7',
+        lightText: '#1a202c',
+      },
+    },
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [],
+};
